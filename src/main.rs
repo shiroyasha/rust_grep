4dd9efd850030eea1a624f27331a63225044c7fe
Igor Sarcevic <igisar@gmail.com>
2014-12-17 00:52:30 +0100

    Set exit status according to the result

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
 use std::os;
-use std::io::File;
-use std::io::BufferedReader;
+use std::io::{File, BufferedReader};
 use std::io::fs;
 use std::io::fs::PathExtensions;
 
-fn search_string(text_to_find: &str, path: Path) {
-
-    let mut file = BufferedReader::new(File::open(&path));
+fn search_in_file(text_to_find: &str, path: Path) -> bool {
+    let mut file  = BufferedReader::new(File::open(&path));
+    let mut found = false;
 
     for line in file.lines().filter_map(|result| result.ok()) {
         if line.as_slice().contains(text_to_find) {
-            println!("{}", path.display());
+            println!("{}", path.display())
+            found = true
         }
     }
+
+    found
 }
 
-fn search_in_files(text_to_find: &str, mut paths: fs::Directories) {
-    for path in paths {
-        if path.is_file() {
-            search_string(text_to_find, path);
-        }
+fn search_in_files(text_to_find: &str, paths: fs::Directories) -> bool {
+    let mut found = false;
+
+    for path in paths.filter(|path| path.is_file()) {
+        if search_in_file(text_to_find, path) { found = true }
     }
+
+    found
 }
 
 fn main() {
     let args = os::args();
     let text_to_find = args.get(1).unwrap().as_slice();
-   
-    match std::io::fs::walk_dir(&Path::new(".")) {
-        Err(why) => println!("! {}", why.kind),
+
+    let found = match std::io::fs::walk_dir(&Path::new(".")) {
+        Err(why) => {
+            println!("! {}", why.kind);
+            false
+        },
         Ok(paths) => search_in_files(text_to_find, paths)
-    }
+    };
+
+    std::os::set_exit_status(if found { 0 } else { 1 })
 }
